2dcfa7a1a4e6b1c00c00fe35dc93d0257d71945c
Aramex <slavbez@mail.ru>
2018-10-10 00:22:17 +0300

    logic start

--- js/file1.js
+++ js/file1.js
@@ -15,22 +15,38 @@ $(document).ready(function () {
         });
         $('.videosliderwrap div:eq(1)').addClass('secondslider');
 
-        $('.homeblock').hide();
-        $('.header-bottom').hide();
-        $('.authorization-block').hide();
-        $('.aboutus').hide();
-        $('.contact').hide();
-        $('.blogpagewrap').hide();
-        $('.blogfull').filter(':eq(0),:eq(1),:eq(2),:eq(3),:eq(4)').hide();
-        $('.blog').hide();
-
-
-
-
-
-
-
-
+        //$('.homeblock').hide();
+        //$('.header-bottom').hide();
+        $('.services, .blog, .blogfull, .contact, .aboutus, .authorization-block').hide();
+        //top nav menu
+       var services = $('.nav-top li:eq(0)');
+       var blog = $('.nav-top li:eq(1)');
+       var aboutUs = $('.nav-top li:eq(2)');
+       var contacts = $('.nav-top li:eq(3)');
+       $(services).click(function () {
+         $('.homeblock, .header-bottom, .blog, .services, .aboutus, .contact').hide();
+         $('.services').fadeIn(1000);
+       });
+       $(blog).click(function () {
+          $('.services, .aboutus, .header-bottom, .homeblock, .contact').hide();
+          $('.blog').fadeIn(1000);
+       });
+       $(aboutUs).click(function () {
+           $('.homeblock, .header-bottom, .blog, .services, .contact, .aboutus').hide();
+           $('.aboutus').fadeIn(1000);
+       });
+       $(contacts).click(function () {
+          $('.homeblock, .header-bottom, .blog, .services, .aboutus').hide();
+          $('.contact').fadeIn(1000);
+       });
+        $('.logo img').click(function () {
+          $('.services, .blog, .blogfull, .contact, .aboutus, .authorization-block').hide();
+          $('.homeblock, .header-bottom').fadeIn(1000);
+        });
+        $('.home').click(function () {
+            $('.services, .blog, .blogfull, .contact, .aboutus, .authorization-block').hide();
+            $('.homeblock, .header-bottom').fadeIn(1000);
+        });
 
 
 
