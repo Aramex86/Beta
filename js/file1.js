ae6cc908eb8899408a67d541771bbdbc979e15f2
Aramex <slavbez@mail.ru>
2018-10-25 00:01:32 +0300

    MPA 1

--- js/file1.js
+++ js/file1.js
@@ -14,32 +14,18 @@ $(document).ready(function () {
             slideMargin:28,
         });
         $('.videosliderwrap div:eq(1)').addClass('secondslider');
-
-        //top nav ,header,authorization
-      /*  var services = $('.nav-top li:eq(0)');
-        var blog = $('.nav-top li:eq(1)');
-        var aboutUs = $('.nav-top li:eq(2)');
-        var contacts = $('.nav-top li:eq(3)');
-        var home = $('.homeblock, .header-bottom');
-        */
-       //$('.services, .blog, .homeblock, .header-bottom, .contact, .aboutus, .authorization-block').hide();
-        /*$('.part.homeblock-page').show();
-        $('.page-link').click(function (event) {
-           event.preventDefault();
-           var name = this.dataset.page;
-           $('.part').hide();
-           $(home).hide();
-           $('.'+ name + '-page').fadeIn(500);
-           $('.logo').click(function () {
-              $(home).fadeIn(500);
-               $('.part').hide();
-           });
-           $('.home').click(function () {
-               $(home).fadeIn(500);
-               $('.part').hide();
-           });
-       });
-*/
+        $(".site-wrap").delay(300).animate({"opacity": "1.5"}, 600);
+        function videoChange(){
+            $('.cover1').click(function () {
+                $('#video2').hide();
+                $('#video1').show();
+            });
+            $('.cover2').click(function () {
+               $('#video1').hide();
+               $('#video2').show();
+            });
+        }
+        videoChange();
 
 
 
@@ -47,31 +33,6 @@ $(document).ready(function () {
 
 
 
-
-      /* $(services).click(function () {
-         $('.homeblock, .header-bottom, .blog, .services, .aboutus, .contact').hide();
-         $('.services').fadeIn(1000);
-       });
-       $(blog).click(function () {
-          $('.services, .aboutus, .header-bottom, .homeblock, .contact').hide();
-          $('.blog').fadeIn(1000);
-       });
-       $(aboutUs).click(function () {
-           $('.homeblock, .header-bottom, .blog, .services, .contact, .aboutus').hide();
-           $('.aboutus').fadeIn(1000);
-       });
-       $(contacts).click(function () {
-          $('.homeblock, .header-bottom, .blog, .services, .aboutus').hide();
-          $('.contact').fadeIn(1000);
-       });
-        $('.logo img').click(function () {
-          $('.services, .blog, .blogfull, .contact, .aboutus, .authorization-block').hide();
-          $('.homeblock, .header-bottom').fadeIn(1000);
-        });
-        $('.home').click(function () {
-            $('.services, .blog, .blogfull, .contact, .aboutus, .authorization-block').hide();
-            $('.homeblock, .header-bottom').fadeIn(1000);
-        });*/
         $('.personal-cab p').click(function () {
              $('.services, .blog, .blogfull, .contact, .aboutus, .homeblock, .header-bottom ').hide();
              $('.authorization-block').fadeIn(500);
