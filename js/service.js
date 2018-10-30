7d47370fe1e251a18e8213eb657d13be6639cee1
Aramex <slavbez@mail.ru>
2018-10-30 11:59:32 +0300

    service js

--- js/service.js
+++ js/service.js
@@ -97,9 +97,129 @@
            $('.row3 ').hide();
            $('.row4 ').hide();
         });
+        $(bottomtest[1]).click(function () {
+            $('.table').show();
+            $('.row1 td:eq(0)').html('Epstein-Barr virus');
+            $('.row2').hide();
+            $('.row3 ').hide();
+            $('.row4 ').hide();
+        });
+        $(bottomtest[2]).click(function () {
+            $('.table').show();
+            $('.row1 td:eq(0)').html('Rotavirus');
+            $('.row2').hide();
+            $('.row3 ').hide();
+            $('.row4 ').hide();
+        });
+        $(bottomtest[3]).click(function () {
+            $('.table').show();
+            $('.row1 td:eq(0)').html('Hepatitis');
+            $('.row2').hide();
+            $('.row3 ').hide();
+            $('.row4 ').hide();
+        });
+        $(bottomtest[4]).click(function () {
+            $('.table').show();
+            $('.row1 td:eq(0)').html('Genital warts (human papillomavirus, or HPV)');
+            $('.row2').hide();
+            $('.row3 ').hide();
+            $('.row4 ').hide();
+        });
+        $(bottomtest[5]).click(function () {
+            $('.table').show();
+            $('.row1 td:eq(0)').html('Influenza (flu)');
+            $('.row2').hide();
+            $('.row3 ').hide();
+            $('.row4 ').hide();
+        });
+        $(bottomtest[6]).click(function () {
+            $('.table').show();
+            $('.row1 td:eq(0)').html('Blood glucose test');
+            $('.row2').hide();
+            $('.row3 ').hide();
+            $('.row4 ').hide();
+        });
+        $(bottomtest[7]).click(function () {
+            $('.table').show();
+            $('.row1 td:eq(0)').html('Cardiac enzymes');
+            $('.row2').hide();
+            $('.row3 ').hide();
+            $('.row4 ').hide();
+        });
+        $(bottomtest[8]).click(function () {
+            $('.table').show();
+            $('.row1 td:eq(0)').html('Calcium blood test');
+            $('.row2').hide();
+            $('.row3 ').hide();
+            $('.row4 ').hide();
+        });
+        $(bottomtest[9]).click(function () {
+            $('.table').show();
+            $('.row1 td:eq(0)').html('C-reactive protein (CRP) test<');
+            $('.row2').hide();
+            $('.row3 ').hide();
+            $('.row4 ').hide();
+        });
+        $(bottomtest[10]).click(function () {
+            $('.table').show();
+            $('.row1 td:eq(0)').html('Full blood count');
+            $('.row2').hide();
+            $('.row3 ').hide();
+            $('.row4 ').hide();
+        });
+        $(bottomtest[11]).click(function () {
+            $('.table').show();
+            $('.row1 td:eq(0)').html('Liver function tests');
+            $('.row2').hide();
+            $('.row3 ').hide();
+            $('.row4 ').hide();
+        });
+        $(bottomtest[12]).click(function () {
+            $('.table').show();
+            $('.row1 td:eq(0)').html('Chlamydia Testing');
+            $('.row2').hide();
+            $('.row3 ').hide();
+            $('.row4 ').hide();
+        });
+        $(bottomtest[13]).click(function () {
+            $('.table').show();
+            $('.row1 td:eq(0)').html('Gonorrhea Testing');
+            $('.row2').hide();
+            $('.row3 ').hide();
+            $('.row4 ').hide();
+        });
+        $(bottomtest[14]).click(function () {
+            $('.table').show();
+            $('.row1 td:eq(0)').html('Syphilis Tests');
+            $('.row2').hide();
+            $('.row3 ').hide();
+            $('.row4 ').hide();
+        });
+        $(bottomtest[15]).click(function () {
+            $('.table').show();
+            $('.row1 td:eq(0)').html('HPV Test');
+            $('.row2').hide();
+            $('.row3 ').hide();
+            $('.row4 ').hide();
+        });
+        $(bottomtest[16]).click(function () {
+            $('.table').show();
+            $('.row1 td:eq(0)').html('Herpes Testing');
+            $('.row2').hide();
+            $('.row3 ').hide();
+            $('.row4 ').hide();
+        });
+        $(bottomtest[16]).click(function () {
+            $('.table').show();
+            $('.row1 td:eq(0)').html('HIV Antibody Test');
+            $('.row2').hide();
+            $('.row3 ').hide();
+            $('.row4 ').hide();
+        });
     }
     }
     bottomList();
+    console.log(bottomList);
 
 
 
